b06e47bb2206749f1f3bcb1077de3c02563d75e3
freddy padilla <fred.padilla50@gmail.com>
2017-02-18 18:04:31 -0800

    Practiced on main

--- TitanSurge/src/Common/Main.java
+++ TitanSurge/src/Common/Main.java
@@ -1,14 +1,22 @@
 package Common;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		double d = (Math.random()*100);
+		Random rand = new Random();
+		int d = rand.nextInt(8);
 	
-		for(int i = 0; i < 5; i+=1){
-			d= Math.random()*100;
+	
+		for(int i = 0; i < 10; i++){
+			d = rand.nextInt(8);
 			System.out.println(d);
 		}
+		
+		
+
+
 
 	}
 
